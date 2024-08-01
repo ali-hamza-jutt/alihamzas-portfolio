4caaf7d76b993f5f39836104d783b345f3b5c138
ali-hamza-jutt <alihamza16jutt@outlook.com>
2024-08-02 01:36:05 +0500

    Add project to Portfolio

--- src/components/Work/ProjectData.jsx
+++ src/components/Work/ProjectData.jsx
@@ -1,11 +1,8 @@
 import Work1 from "../../assets/work1.png";
 import Work2 from "../../assets/work2.png";
-import Work3 from "../../assets/work3.png";
-import Work4 from "../../assets/work4.png";
-import Work5 from "../../assets/work5.png";
-import Work6 from "../../assets/work6.png";
 import Work7 from "../../assets/work7.gif";
 import Work8 from '../../assets/work8.png';
+import Work9 from '../../assets/work9.png'
 
 export const projectsData = [
   {
@@ -30,37 +27,6 @@ export const projectsData = [
     link: "https://netflix-clone-jinix.web.app/",
   },
   {
-    id: 3,
-    image: Work3,
-    title:
-      "CryptoRank: The Ultimate Cryptocurrency App for Real-Time Market Analysis",
-    category: "frontend",
-    link: "https://my-crypto-rank.vercel.app/",
-  },
-  {
-    id: 4,
-    image: Work4,
-    title:
-      "VR Oasis - Building a Professional VR Web App with Chakra UI and React",
-    category: "design",
-    link: "https://vr-web-app.netlify.app/",
-  },
-  {
-    id: 5,
-    image: Work5,
-    title: "Modern Tech Agency Web App Built with React and Responsive Design",
-    category: "design",
-    link: "https://tech-agency-web.netlify.app/",
-  },
-  {
-    id: 6,
-    image: Work6,
-    title:
-      "Building Dynamic Web Pages with Redux Toolkit: A High-Performing Approach to State Management",
-    category: "frontend",
-    link: "https://main--my-redux-shopping-cart.netlify.app/",
-  },
-  {
     id: 8,
     image: Work8,
     title:
@@ -68,11 +34,19 @@ export const projectsData = [
     category: "frontend",
     link: "https://ecommerce-admin-board-seven.vercel.app/",
   },
+  {
+    id: 9,
+    image: Work9,
+    title:
+      "Create a responsive Pinterest-style landing page using React.js, Styled Components and the Unsplash API to fetch Images",
+    category: "frontend",
+    link: "https://pinterest-landing-page.vercel.app/",
+  },
 ];
 
 export const projectNav = [
   { name: "all" },
   { name: "full stack" },
   { name: "frontend" },
-  { name: "design" },
+  // { name: "design" },
 ];
