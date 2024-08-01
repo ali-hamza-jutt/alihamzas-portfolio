95fe278642ea38f5cb6269064a7f190c9fb7a0b4
ali-hamza-jutt <alihamza16jutt@outlook.com>
2024-08-01 18:46:57 +0500

    Add Testimonial Section to Portfolio
    - Created testimonial section
    - Added testimonial of my past clients

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Skills from './components/Skills/Skills';
 import Services from './components/Services/Services';
 import Qualification from './components/Qualification/Qualification';
 import Work from './components/Work/Work';
+import Testimonial from './components/Testimonial/Testimonial';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Services/>
       <Qualification/>
       <Work/>
+      <Testimonial/>  
     </main>
     </>
   );
