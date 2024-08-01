1d415d12c4e5bd90493a19fde6310b9883898915
ali-hamza-jutt <alihamza16jutt@outlook.com>
2024-08-01 16:31:02 +0500

    Add skills section to portfolio
    - Created a Skills Section for my Portfolio
    - Included BackEnd and FrontEnd Skills

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import FrontEnd from "./FrontEnd";
+import BackEnd from "./BackEnd";
+import "./Skills.css";
+
+const Skills = () => {
+  return (
+    <section className="skills section" id="skills">
+      <h2 className="section__title">Skills</h2>
+      <span className="section__subtitle">My Technical Skills</span>
+
+      <div className="skills__container container grid">
+        <FrontEnd />
+        <BackEnd />
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
