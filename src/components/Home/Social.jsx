e728d520ca3066eb152214fd56f59fcb84b9d5e5
ali-hamza-jutt <alihamza16jutt@outlook.com>
2024-08-01 16:08:18 +0500

    Created Home Section
    Home Section created that contain profile picture, name, title and short summary
    
    Scroll component with mouse scoll icon which allows to scroll down the app
    
    Social component that contain Github, Instagram and Linkedin profile links

---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/jitendra.khadoliya"
+        className="home__social-icon"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faInstagram} />
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/alihamza7811/"
+        className="home__social-icon"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faLinkedin} />
+      </a>
+
+      <a
+        href="https://github.com/ali-hamza-jutt"
+        className="home__social-icon"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faGithub} />
+      </a>
+    </div>
+  );
+};
+
+export default Social;
