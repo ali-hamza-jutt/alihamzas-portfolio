24abcf19d0d29489a479a65714478b79c600501f
ali-hamza-jutt <alihamza16jutt@outlook.com>
2024-08-01 22:27:36 +0500

    Add testimonials

--- src/components/Testimonial/ClientsInfo.js
+++ src/components/Testimonial/ClientsInfo.js
@@ -2,33 +2,41 @@ import Image1 from "../../assets/testimonial1.jpg";
 import Image2 from "../../assets/testimonial2.jpg";
 import Image3 from "../../assets/testimonial3.jpg";
 import Image4 from "../../assets/testimonial4.jpg";
+import Image5 from "../../assets/testimonial4.jpg";
 
 export const ClientsInfo = [
   {
     id: 1,
     image: Image1,
-    title: "Adesh",
+    title: "Abdullah",
     description:
-      "Jitendra is an exceptional React developer. He delivered high-quality work on time and within budget. I highly recommend him to anyone looking for a skilled front end developer",
+      "If you want a javaScript developer who truly cares about your project, look no further than Ali. He was professional, responsive, and always went the extra mile to ensure success.",
   },
   {
     id: 2,
     image: Image2,
-    title: "Ankit",
+    title: "Mohsin",
     description:
-      "If you want a javaScript developer who truly cares about your project, look no further than Jitendra. He was professional, responsive, and always went the extra mile to ensure success.",
+      "I recently had a project developed with Ali, and I'm highly impressed with his work. Their attention to detail, responsiveness, and technical expertise exceeded my expectations. I highly recommend his services for any development needs.",
   },
   {
     id: 3,
     image: Image3,
-    title: "Punit",
+    title: "Hunain",
     description:
       "He impressed me with their javaScript skills. he delivered high-quality work on time and within budget. I highly recommend jitendra to anyone looking for a talented react developer.",
   },
   {
     id: 4,
     image: Image4,
-    title: "Dinesh",
+    title: "Mehdi",
+    description:
+      "He turned my vision into a reality, creating a website that exceeded my expectations. he has deep expertise in javaScript and seamlessly integrated complex features.",
+  },
+  {
+    id: 4,
+    image: Image5,
+    title: "Moiz",
     description:
       "He turned my vision into a reality, creating a website that exceeded my expectations. he has deep expertise in javaScript and seamlessly integrated complex features.",
   },
